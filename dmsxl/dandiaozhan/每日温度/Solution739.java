65fabcb09ae88756ede97854f98cf273e9e96937
hdusername <1667311156@qq.com>
2024-06-09 19:48:25 +0800

    代码随想录关于单调栈的算法题

---
+++ dmsxl/dandiaozhan/每日温度/Solution739.java
@@ -0,0 +1,62 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+* https://programmercarl.com/0739.%E6%AF%8F%E6%97%A5%E6%B8%A9%E5%BA%A6.html#%E6%80%9D%E8%B7%AF
+ *
+ *请根据每日 气温 列表，重新生成一个列表。对应位置的输出为：要想观测到更高的气温，至少需要等待的天数。如果气温在这之后都不会升高，请在该位置用 0 来代替。
+ * 例如，给定一个列表 temperatures = [73, 74, 75, 71, 69, 72, 76, 73]，你的输出应该是 [1, 1, 4, 2, 1, 1, 0, 0]。
+ * 提示：气温 列表长度的范围是 [1, 30000]。每个气温的值的均为华氏度，都是在 [30, 100] 范围内的整数。
+ */
+public class Solution739 {
+
+    public int[] dailyTemperatures(int[] temperatures) {
+
+        int lens=temperatures.length;
+        int []res=new int[lens];
+
+        /**
+         如果当前遍历的元素 大于栈顶元素，表示 栈顶元素的 右边的最大的元素就是 当前遍历的元素，
+         所以弹出 栈顶元素，并记录
+         如果栈不空的话，还要考虑新的栈顶与当前元素的大小关系
+         否则的话，可以直接入栈。
+         注意，单调栈里 加入的元素是 下标。
+         */
+        Deque<Integer> stack=new LinkedList<>();
+        stack.push(0);
+        for(int i=1;i<lens;i++){
+
+            if(temperatures[i]<=temperatures[stack.peek()]){
+                stack.push(i);
+            }else{
+                while(!stack.isEmpty()&&temperatures[i]>temperatures[stack.peek()]){
+                    res[stack.peek()]=i-stack.peek();
+                    stack.pop();
+                }
+                stack.push(i);
+            }
+        }
+
+        return  res;
+    }
+
+    //--------这 是一条分界线
+    // 版本 2
+    class Solution {
+        public int[] dailyTemperatures(int[] temperatures) {
+            int lens=temperatures.length;
+            int []res=new int[lens];
+            Deque<Integer> stack=new LinkedList<>();
+            for(int i=0;i<lens;i++){
+
+                while(!stack.isEmpty()&&temperatures[i]>temperatures[stack.peek()]){
+                    res[stack.peek()]=i-stack.peek();
+                    stack.pop();
+                }
+                stack.push(i);
+            }
+
+            return  res;
+        }
+    }
+}
