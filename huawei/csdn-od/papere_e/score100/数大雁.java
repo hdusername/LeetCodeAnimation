22de2833ad408edb854f10ce4372297bbf94a0b6
hdusername <1667311156@qq.com>
2025-01-08 23:56:55 +0800

    华为od-E卷-数大雁

---
+++ huawei/csdn-od/papere_e/score100/数大雁.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+/**
+ * 一群大雁往南飞，给定一个字符串记录地面上的游客听到的大雁叫声，请给出叫声最少由几只大雁发出。
+ * 具体的：
+ * 大雁发出的完整叫声为”quack“，因为有多只大雁同一时间嘎嘎作响，所以字符串中可能会混合多个”quack”。
+ * 大雁会依次完整发出”quack”，即字符串中’q’ ,‘u’, ‘a’, ‘c’, ‘k’ 这5个字母按顺序完整存在才能计数为一只大雁。如果不完整或者没有按顺序则不予计数。
+ * 如果字符串不是由’q’, ‘u’, ‘a’, ‘c’, ‘k’ 字符组合而成，或者没有找到一只大雁，请返回-1。
+ *
+ * 输入描述：
+ * 一个字符串，包含大雁quack的叫声。1 <= 字符串长度 <= 1000，字符串中的字符只有’q’, ‘u’, ‘a’, ‘c’, ‘k’。
+ *
+ * 输出描述：
+ * 大雁的数量
+ */
+//todo: 这个方法并不能通过所有用例
+public class 数大雁 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        char[] charArray = s.toCharArray();
+        int count=0;
+        while (find(charArray)){
+            count++;
+        }
+
+        System.out.println(count==0?-1:count);
+
+    }
+
+    /**
+     * 功能：一只大雁叫完一次，紧接着会叫下一次
+     * @param charArray
+     * @return
+     */
+    public static boolean find(char[] charArray){
+        boolean flag = false;
+        //此角标记录叫声quack的角标值
+        int index = 0;
+        //保存在字符串种找到的叫声值的角标
+        int[] ans = new int[5];
+        for(int i=0; i<charArray.length; i++){
+            if(charArray[i] == "quack".charAt(index)){
+                ans[index++] = i;
+            }
+
+            if(index == 5){
+                //说明找到了quack这几个值了
+                for(int j =0; j<ans.length; j++){
+                    //为了再次调用find方法时，不再能与quack进行对应
+                    charArray[ans[j]]=' ';
+                }
+                index = 0;
+                //不用新建数组了，因为index已经置为0了
+                //ans = new int[5];
+                flag=true;
+            }
+
+        }
+        return flag;
+    }
+}
