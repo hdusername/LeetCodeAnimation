accb40e9b37d386abecbe1b2cecdfb22728f3a19
hdusername <1667311156@qq.com>
2025-02-11 09:48:12 +0800

    华为od-E卷-ai面板识别

---
+++ huawei/csdn-od/papere_e/score100/ai面板识别.java
@@ -0,0 +1,98 @@
+import java.util.*;
+
+/**
+ * AI识别到面板上有 N（1 ≤ N ≤ 100）个指示灯，灯大小一样，任意两个之间无重叠。由于 AI 识别误差，每次别到的指示灯位置可能有差异，以4个坐标值
+ * 描述AI识别的指示灯的大小和位置(左上角x1,y1，右下角x2,y2)，
+ * 请输出先行后列排序的指示灯的编号，排序规则：
+ * 每次在尚未排序的灯中挑选最高的灯作为的基准灯，
+ * 找出和基准灯属于同一行所有的灯进行排序。两个灯高低偏差不超过灯半径算同一行（即两个灯坐标的差 ≤ 灯高度的一半）。
+ *
+ * 输入描述：
+ * 第一行为N，表示灯的个数
+ *  接下来N行，每行为1个灯的坐标信息，格式为：
+ *  编号 x1 y1 x2 y2
+ *  编号全局唯一
+ *  1 ≤ 编号 ≤ 100
+ *  0 ≤ x1 < x2 ≤ 1000
+ *  0  ≤  y1 < y2 ≤ 1000
+ *
+ *  输出描述：
+ *  排序后的编号列表，编号之间以空格分隔
+ *  用例输入：
+ *   5
+ *   1 0 0 2 2
+ *   2 6 1 8 3
+ *   3 3 2 5 4
+ *   5 5 4 7 6
+ *   4 0 4 2 6
+ *
+ *   输出：
+ *   1 2 3 4 5
+ */
+public class ai面板识别 {
+
+    static class Light {
+        int id; // 编号
+        int x; // 圆心横坐标
+        int y; // 圆心纵坐标
+        int r; // 圆半径
+
+        public Light(int id, int x, int y, int r) {
+            this.id = id;
+            this.x = x;
+            this.y = y;
+            this.r = r;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Light> lightList = new ArrayList<>();
+
+        int n = scanner.nextInt();
+        for(int i=0; i<n; i++){
+           int id = scanner.nextInt();
+           int lx = scanner.nextInt();
+           int ly = scanner.nextInt();
+           int rx = scanner.nextInt();
+           int ry = scanner.nextInt();
+           Light light = new Light(id, lx+((rx-lx)/2), ly+((ry-ly)/2), (rx-lx)/2);
+           lightList.add(light);
+
+        }
+
+        //按照高度排序，高度小的排在前面
+        lightList.sort((a, b)->a.y-b.y);
+
+        //保存同一行的灯
+        ArrayList<Light> sameLineList = new ArrayList<>();
+
+        //首先取出一个基准灯
+        Light baseLight = lightList.get(0);
+        sameLineList.add(baseLight);
+        StringJoiner joiner = new StringJoiner(" ");
+        for(int i=1; i<lightList.size(); i++){
+            if(Math.abs(baseLight.y-lightList.get(i).y)<= baseLight.r){
+                //同一行
+                sameLineList.add(lightList.get(i));
+
+            }else {
+
+                //同一行的还要按照从左到右的顺序进行排序，因为已经按照高度排序了，所以只要走到这一步，就说明后续元素肯定没有和基准灯一行的了
+                sameLineList.sort((a,b)->(a.x-b.x));
+                sameLineList.forEach(a->joiner.add(a.id+""));
+                sameLineList.clear();
+                baseLight = lightList.get(i);
+                sameLineList.add(baseLight);
+            }
+        }
+
+        //说明最后还有元素没有输出
+        if(sameLineList.size() !=0){
+            sameLineList.sort((a,b)->(a.x-b.x));
+            sameLineList.forEach(a->joiner.add(a.id+""));
+        }
+
+        System.out.println(joiner);
+    }
+}
