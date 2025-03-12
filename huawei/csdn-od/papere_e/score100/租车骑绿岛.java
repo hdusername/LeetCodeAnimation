8143138aad2eade8821fe4544afd53cfe88214c9
hdusername <1667311156@qq.com>
2025-03-12 20:44:20 +0800

    华为od-E卷-score100-租车骑绿岛,新增标准答案

--- huawei/csdn-od/papere_e/score100/租车骑绿岛.java
+++ huawei/csdn-od/papere_e/score100/租车骑绿岛.java
@@ -56,3 +56,36 @@ public class 租车骑绿岛 {
 
     }
 }
+
+//标准答案
+//import java.util.Arrays;
+//import java.util.Scanner;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//
+//        int m = sc.nextInt();
+//        int n = sc.nextInt();
+//
+//        int[] weights = new int[n];
+//        for (int i = 0; i < n; i++) {
+//            weights[i] = sc.nextInt();
+//        }
+//
+//        Arrays.sort(weights);
+//
+//        int count = 0;
+//
+//        int i = 0;
+//        int j = n - 1;
+//
+//        while (i <= j) {
+//            if (weights[i] + weights[j] <= m) i++;
+//            j--;
+//            count++;
+//        }
+//
+//        System.out.println(count);
+//    }
+//}
