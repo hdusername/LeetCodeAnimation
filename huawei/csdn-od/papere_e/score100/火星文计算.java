81f12ce814952f1b3f1088654b811084dd953817
hdusername <1667311156@qq.com>
2025-03-12 23:40:28 +0800

    华为od-E卷-score100-火星文计算,新增标准答案

--- huawei/csdn-od/papere_e/score100/火星文计算.java
+++ huawei/csdn-od/papere_e/score100/火星文计算.java
@@ -90,3 +90,36 @@ public class 火星文计算 {
 
     }
 }
+
+//标准答案
+//import java.util.Arrays;
+//import java.util.Scanner;
+//import java.util.regex.Matcher;
+//import java.util.regex.Pattern;
+//
+//public class Main {
+//  public static void main(String[] args) {
+//    Scanner sc = new Scanner(System.in);
+//    String str = sc.next();
+//    System.out.print(getResult(str));
+//  }
+//
+//  public static long getResult(String str) {
+//    Pattern p = Pattern.compile("(\\d+)#(\\d+)");
+//
+//    while (true) {
+//      Matcher m = p.matcher(str);
+//      if (!m.find()) break;
+//
+//      String subStr = m.group(0);
+//      long x = Long.parseLong(m.group(1));
+//      long y = Long.parseLong(m.group(2));
+//      str = str.replaceFirst(subStr, 4 * x + 3 * y + 2 + "");
+//    }
+//
+//    return Arrays.stream(str.split("\\$"))
+//        .map(Long::parseLong)
+//        .reduce((x, y) -> 2 * x + y + 3)
+//        .orElse(0L);
+//  }
+//}
