66e29196518742ef589bb4f5dc03ed2f0df25077
hdusername <1667311156@qq.com>
2025-03-12 17:59:22 +0800

    华为od-E卷-score100-响应报文时间,新增标准答案

--- huawei/csdn-od/papere_e/score100/响应报文时间.java
+++ huawei/csdn-od/papere_e/score100/响应报文时间.java
@@ -66,3 +66,46 @@ public class 响应报文时间 {
 
     }
 }
+
+//标准答案
+//import java.util.Scanner;
+//
+//public class Main {
+//  public static void main(String[] args) {
+//    Scanner sc = new Scanner(System.in);
+//
+//    int c = sc.nextInt();
+//
+//    int[][] messages = new int[c][2];
+//    for (int i = 0; i < c; i++) {
+//      messages[i][0] = sc.nextInt();
+//      messages[i][1] = sc.nextInt();
+//    }
+//
+//    System.out.println(getResult(messages));
+//  }
+//
+//  public static int getResult(int[][] messages) {
+//    int ans = Integer.MAX_VALUE;
+//
+//    for (int[] message : messages) {
+//      int respTime = message[0] + getMaxResponseTime(message[1]);
+//      ans = Math.min(ans, respTime);
+//    }
+//
+//    return ans;
+//  }
+//
+//  public static int getMaxResponseTime(int m) {
+//    if (m >= 128) {
+//      StringBuilder bStr = new StringBuilder(Integer.toBinaryString(m));
+//
+//      int exp = Integer.parseInt(bStr.substring(1, 4), 2);
+//      int mant = Integer.parseInt(bStr.substring(4), 2);
+//
+//      return (mant | 16) << (exp + 3);
+//    } else {
+//      return m;
+//    }
+//  }
+//}
