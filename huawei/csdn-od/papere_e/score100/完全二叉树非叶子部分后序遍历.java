13e50c910fdba1addd9e0ce3be0b627f12b26b96
hdusername <1667311156@qq.com>
2025-03-13 11:15:21 +0800

    华为od-E卷-score100-完全二叉树非叶子部分后序遍历，新增标准答案

--- huawei/csdn-od/papere_e/score100/完全二叉树非叶子部分后序遍历.java
+++ huawei/csdn-od/papere_e/score100/完全二叉树非叶子部分后序遍历.java
@@ -57,3 +57,43 @@ public class 完全二叉树非叶子部分后序遍历 {
         }
     }
 }
+
+//标准答案
+//import java.util.Scanner;
+//import java.util.StringJoiner;
+//
+//public class Main {
+//    static String[] arr;
+//    static StringJoiner non_leafs = new StringJoiner(" "); // 非叶子节点后序遍历序列
+//
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//
+//        arr = sc.nextLine().split(" ");
+//
+//        if (arr.length == 1) {
+//            // 只有一个节点时，直接输出
+//            System.out.println(arr[0]);
+//        } else {
+//            // 按照后序遍历规则，递归完全二叉树
+//            dfs(0);
+//            System.out.println(non_leafs);
+//        }
+//    }
+//
+//    public static void dfs(int root) {
+//        int left = root * 2 + 1;
+//        int right = root * 2 + 2;
+//
+//        if (arr.length > left) {
+//            // 左：如果左孩子不为空，则说明第i个元素不是叶子，因此继续递归其左孩子，即将左孩子当成新的根来递归
+//            dfs(left);
+//            if (arr.length > right) {
+//                // 右：如果右孩子也不为空，则根据后序遍历原则，还要对右孩子进行递归
+//                dfs(right);
+//            }
+//            // 根
+//            non_leafs.add(arr[root]);
+//        }
+//    }
+//}
